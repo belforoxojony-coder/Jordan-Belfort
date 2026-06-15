f43ab42cf7f3e4d99e9a955fa4ca38322876a9c3
belforoxojony-coder <belforoxojony@gmail.com>
2026-06-14 21:41:57 -0300

    Grafista.py

--- jordan_belfort/agents/grafista.py
+++ jordan_belfort/agents/grafista.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import ccxt.pro as ccxtpro
 import ccxt
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Any
 from jordan_belfort.config import bot_config
 
 logger = logging.getLogger("Grafista")
